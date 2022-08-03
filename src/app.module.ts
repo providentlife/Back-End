835b5c03d5e2f71f994d0a05042a4bd774f27c1c
Matt-Tan15 <tanmatt804@gmail.com>
2022-08-03 14:51:07 -0400

    able to create a table through postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserStartingMetricsModule } from './user-starting-metrics/user-starting-metrics.module';
 import { WeightEntriesModule } from './weight-entries/weight-entries.module';
-
+import entities from './typeorm';
 
 @Module({
-  imports: [UserModule, UserStartingMetricsModule, WeightEntriesModule, TypeOrmModule],
+  imports: [UserModule, UserStartingMetricsModule, WeightEntriesModule, TypeOrmModule.forRoot({
+    type: "postgres",
+    host: 'localhost',
+    port: 5432,
+    username: "postgres",
+    password: "password",
+    database: 'project_health',
+    entities,
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
